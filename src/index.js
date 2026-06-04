3c8224aea6f569e2ba1e746ac660d4090944adfd
Vishal Sinha <vishalsinha15456@gmail.com>
2026-06-04 17:43:31 +0530

    chore: allow all origins in cors configuration

--- src/index.js
+++ src/index.js
@@ -11,14 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  process.env.FRONTEND_URL
-].filter(Boolean);
-
 app.use(cors({
-  origin: allowedOrigins,
+  origin: true,
   credentials: true
 }));
 
